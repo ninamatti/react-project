8a0dc0a492c8e9244c79d6da043672e6fea82f84
Nina Matti <ninadesireematti@gmail.com>
2020-10-21 12:24:42 +0900

    Imported AllPhotos, SinglePhoto and Navbar as well as utility methods
    We created the three states for currentView, photos and selectedPhoto. We also managed to fetch photo data and store it in our photos array.
    
    Co-Authored-By: Yoshiaki Aibara <69150682+Yoshi106@users.noreply.github.com>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/styles.css";
+import Navbar from "./Navbar.jsx";
+import AllPhotos from "./AllPhotos";
+import SinglePhoto from "./SinglePhoto";
+import { listObjects, getSingleObject, saveObject } from "../utils/index.js";
 
 export default function App() {
+  const [currentView, setView] = useState("AllPhotos");
+  let [photos, setPhotoArray] = useState([""]);
+  const [selectedPhoto, setPhoto] = useState(["Photo1"]);
+
+  // define how states change with useEffect?
+  useEffect(() => {
+    async function fetchData() {
+      photos = await listObjects();
+      console.log(photos);
+    }
+    fetchData();
+  }, []);
+
+  /* console.log(photos); */
+
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <div className="app">
+        <h1>Hello World!</h1>
+      </div>
+      <Navbar />
+      {currentView === "AllPhotos" ? <AllPhotos /> : <SinglePhoto />}
+      {/* {currentView !== "AllPhotos" && <SinglePhoto/>} */}
+    </>
   );
 }
