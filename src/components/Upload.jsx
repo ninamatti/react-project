ab6dfeccdbdded0f0bb81d86c3db351786cd7261
Nina Matti <ninadesireematti@gmail.com>
2020-10-21 19:02:35 +0900

    Added reference to input upload input field
    Co-Authored-By: Yoshiaki Aibara <69150682+Yoshi106@users.noreply.github.com>

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import _ from "lodash";
 import "../styles/upload.css";
+import { saveObject } from "../utils";
 
 export default function Upload() {
-  return <div className="file-upload" />;
+  const inputRef = React.useRef(null);
+
+  return (
+    <>
+      <div className="file-upload" />
+      {/* <input type="text" className="input" id="imageInput"/> */}
+
+      <form type="submit" value="Submit">
+        <label>Select a file:</label>
+        <input type="file" id="myfile" name="myfile" ref={inputRef} />
+
+        <button
+          type="submit"
+          onClick={async e => {
+            e.preventDefault();
+            console.log(inputRef.current.files[0]);
+            console.log(inputRef.current.files);
+            saveObject(inputRef.current.files[0]);
+            //console.log(newPhoto);
+          }}
+        >
+          Upload Imageeeee
+        </button>
+      </form>
+    </>
+  );
 }
